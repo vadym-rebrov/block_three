5b31cc4349c16abff1c79d30db1802d7972147f3
Vadym Rebrov <vadym.rebrov@gmail.com>
2025-12-18 13:45:05 +0200

    add movie page

--- src/constants/pagesURLs.js
+++ src/constants/pagesURLs.js
@@ -5,6 +5,7 @@ const result = {
   [pages.defaultPage]: `${config.UI_URL_PREFIX}/${pages.defaultPage}`,
   [pages.login]: `${config.UI_URL_PREFIX}/${pages.login}`,
   [pages.secretPage]: `${config.UI_URL_PREFIX}/${pages.secretPage}`,
+  [pages.moviesPage]: `${config.UI_URL_PREFIX}/${pages.moviesPage}`,
 };
 
 export default result;
---
+++ src/pages/movies/index.jsx
@@ -0,0 +1,18 @@
+import React, { useMemo } from 'react';
+import IntlProvider from 'misc/providers/IntlProvider';
+import useLocationSearch from 'misc/hooks/useLocationSearch';
+
+import getMessages from './intl';
+import Movies from './containers/Movies';
+
+function Index(props) {
+    const { lang } = useLocationSearch();
+    const messages = useMemo(() => getMessages(lang), [lang]);
+    return (
+        <IntlProvider messages={messages}>
+            <Movies {...props} />
+        </IntlProvider>
+    );
+}
+
+export default Index;
