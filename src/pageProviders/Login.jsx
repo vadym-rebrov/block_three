265a3456c4b6a7e1b062b7460d42297d797fe121
Vadym <vadym.rebrov@gmail.com>
2026-01-30 19:06:56 +0200

    add auth logic

--- src/pageProviders/Login.jsx
+++ src/pageProviders/Login.jsx
@@ -1,17 +1,17 @@
 import React, { useEffect } from 'react';
 import useUser from 'misc/hooks/useUser';
 import useChangePage from 'misc/hooks/useChangePage';
-
 import LoginPage from 'pages/login';
 import pageURLs from 'constants/pagesURLs';
-import * as pages from 'constants/pages'
+import * as pages from 'constants/pages';
+
 const Login = () => {
   const { isLogined, isLoaded } = useUser();
   const changePage = useChangePage();
-
+  console.log('IsLogined: ', isLogined);
   useEffect(() => {
     if (isLoaded && isLogined) {
-      changePage({ path: `/${pageURLs[pages.defaultPage]}` });
+      changePage({ path: pageURLs[pages.defaultPage] });
     }
   }, [isLoaded, isLogined, changePage]);
 
@@ -19,7 +19,9 @@ const Login = () => {
     return null;
   }
 
-  return (<LoginPage />);
+  // Показуємо форму входу ТІЛЬКИ якщо юзер НЕ залогінений
+  // Ніякого PageAccessValidator тут не треба!
+  return <LoginPage />;
 };
 
 export default Login;
